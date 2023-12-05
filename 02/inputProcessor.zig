65f49f0de9c83c3dc0b21c7283e68b0d6116c9ea
danielkempenich <dan.kempenich@gmail.com>
2023-12-04 21:13:49 -0600

    Final day 2

--- 02/inputProcessor.zig
+++ 02/inputProcessor.zig
@@ -8,6 +8,7 @@ const GameResult = enum {
 const GameRecord = struct {
     gameNumber: u32,
     gameResult: GameResult,
+    gameMinimum: Round,
 };
 
 const Round = struct {
@@ -24,18 +25,26 @@ const Round = struct {
             round.green = number;
         }
     }
+
+    pub fn init(blue: u32, red: u32, green: u32) Round {
+        return Round{ .blue = blue, .red = red, .green = green };
+    }
 };
 
 test "part one" {
-    try std.testing.expect(std.meta.eql(processLine("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"), GameRecord{ .gameNumber = 1, .gameResult = GameResult.possible }));
-    try std.testing.expect(std.meta.eql(processLine("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"), GameRecord{ .gameNumber = 2, .gameResult = GameResult.possible }));
-    try std.testing.expect(std.meta.eql(processLine("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"), GameRecord{ .gameNumber = 3, .gameResult = GameResult.notPossible }));
-    try std.testing.expect(std.meta.eql(processLine("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"), GameRecord{ .gameNumber = 4, .gameResult = GameResult.notPossible }));
-    try std.testing.expect(std.meta.eql(processLine("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), GameRecord{ .gameNumber = 5, .gameResult = GameResult.possible }));
+    try std.testing.expectEqual(GameRecord{ .gameNumber = 1, .gameResult = GameResult.possible, .gameMinimum = Round.init(6, 4, 2) }, processLine("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"));
+    try std.testing.expectEqual(GameRecord{ .gameNumber = 2, .gameResult = GameResult.possible, .gameMinimum = Round.init(4, 1, 3) }, processLine("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"));
+    try std.testing.expectEqual(GameRecord{ .gameNumber = 3, .gameResult = GameResult.notPossible, .gameMinimum = Round.init(6, 20, 13) }, processLine("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"));
+    try std.testing.expectEqual(GameRecord{ .gameNumber = 4, .gameResult = GameResult.notPossible, .gameMinimum = Round.init(15, 14, 3) }, processLine("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"));
+    try std.testing.expectEqual(GameRecord{ .gameNumber = 5, .gameResult = GameResult.possible, .gameMinimum = Round.init(2, 6, 3) }, processLine("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"));
 }
 
 pub fn processLine(line: []const u8) GameRecord {
-    var gameRecord: GameRecord = GameRecord{ .gameNumber = 0, .gameResult = GameResult.possible };
+    var gameRecord: GameRecord = GameRecord{
+        .gameNumber = 0,
+        .gameResult = GameResult.possible,
+        .gameMinimum = Round.init(0, 0, 0),
+    };
     var pos: u32 = 0;
 
     const roundUpperLimit = Round{ .blue = 14, .red = 12, .green = 13 };
@@ -67,7 +76,16 @@ pub fn processLine(line: []const u8) GameRecord {
             (round.green > roundUpperLimit.green))
         {
             gameRecord.gameResult = GameResult.notPossible;
-            break;
+        }
+
+        if (round.blue > gameRecord.gameMinimum.blue) {
+            gameRecord.gameMinimum.blue = round.blue;
+        }
+        if (round.red > gameRecord.gameMinimum.red) {
+            gameRecord.gameMinimum.red = round.red;
+        }
+        if (round.green > gameRecord.gameMinimum.green) {
+            gameRecord.gameMinimum.green = round.green;
         }
     }
 
@@ -84,6 +102,7 @@ pub fn main() !void {
 
     var buf: [1024]u8 = undefined;
     var sumTotal: u32 = 0;
+    var powerTotal: u32 = 0;
     var linesRead: u32 = 0;
     while (try inStream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         linesRead += 1;
@@ -95,6 +114,7 @@ pub fn main() !void {
             },
             GameResult.notPossible => {},
         }
+        powerTotal += (gameRecord.gameMinimum.blue * gameRecord.gameMinimum.red * gameRecord.gameMinimum.green);
     }
-    std.debug.print("sumTotal: {d} for {d} lines\n", .{ sumTotal, linesRead });
+    std.debug.print("sumTotal: {d} powerTotal: {d} for {d} lines\n", .{ sumTotal, powerTotal, linesRead });
 }
